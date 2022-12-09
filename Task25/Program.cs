756fdfd61a2b07f5e7f043807e247162c6e343ec
Elena1011 <116499734+Elena1011@users.noreply.github.com>
2022-12-09 03:05:43 +0300

    Task 25, 27, 29

---
+++ Task25/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход 
+// два числа (A и B) и возводит число A в натуральную степень B.
+
+// 3, 5 -> 243
+
+// 2, 4 -> 16
+
+Console.WriteLine("Введите число А:");
+double a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число В (натуральное):");
+double b = Convert.ToInt32(Console.ReadLine());
+
+if (b < 0) Console.WriteLine("Число В не является натуральным");
+else Console.WriteLine($"{a}, {b} -> {Exponentiation(a, b)}");
+
+
+double Exponentiation(double n1, double n2)
+{
+    return Math.Pow(n1, n2);
+}
+
+
---
+++ Task29/Program.cs
@@ -0,0 +1,58 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+// 1, 2, 5, 7, 19, 6, 1, 33 -> [1, 2, 5, 7, 19, 6, 1, 33]
+
+
+void PrintMass(int[] array)
+{
+    int count = array.Length;
+    for (int i = 0; i < count; i++)
+    {
+        if (i < count - 1) Console.Write($"{array[i]}, ");
+        else Console.Write(array[i]);
+    }
+}
+void FillArray(int[] arr, int a, int b)
+{
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = new Random().Next(a, b);
+    }
+}
+
+Console.WriteLine("Введите кол-во элементов: ");
+int count = Math.Abs(Convert.ToInt32(Console.ReadLine()));
+int[] myArray = new int[count];
+
+Console.WriteLine($"Введите диапазон для значений массива: ");
+
+Console.Write($"От:");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write($"До:");
+int num2 = Convert.ToInt32(Console.ReadLine());
+bool IsTrue = true;
+
+while (IsTrue)
+{
+    if (num2 > num1)
+    {
+        FillArray(myArray, num1, num2);
+        PrintMass(myArray);
+        Console.Write("  ->  ");
+        Console.Write("[");
+        PrintMass(myArray);
+        Console.Write("]");
+        IsTrue = false;
+    }
+    else
+    {
+        Console.WriteLine("Неверный диапазон, 'От' должно быть меньше чем 'До'");
+        Console.WriteLine("Введите диапазон еще раз ");
+        Console.Write($"От:");
+        num1 = Convert.ToInt32(Console.ReadLine());
+        Console.Write($"До:");
+        num2 = Convert.ToInt32(Console.ReadLine());
+    }
+}
